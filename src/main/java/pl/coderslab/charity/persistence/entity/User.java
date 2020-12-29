5a93526ed4799e917e49029d16a3c0a36535d379
Your Name <szymek.fafrowicz@gmail.com>
2020-12-29 16:31:25 +0100

    Added message to registration page.

--- src/main/java/pl/coderslab/charity/persistence/entity/User.java
+++ src/main/java/pl/coderslab/charity/persistence/entity/User.java
@@ -33,6 +33,7 @@ public class User {
     private String password;
 
     private int enabled;
+
     @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(name = "users_roles", joinColumns = @JoinColumn(name = "user_id"),
             inverseJoinColumns = @JoinColumn(name = "role_id"))
